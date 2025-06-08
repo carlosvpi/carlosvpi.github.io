2f9b32be76edb065b8994c59f1ff0ec463bfc7a8
Carlos Vazquez <carlos.vazpi@gmail.com>
2025-06-08 21:56:54 +0200

    Added ul

--- index.js
+++ index.js
@@ -1,16 +1,28 @@
 const f = async () => {  
-  const response = await fetch('http://localhost:3000/addPlayer', {
+  
+}
+
+window.addEventListener('load', async () => {
+  const responseAddPlayer = await fetch('http://localhost:3000/addPlayer', {
     method: 'POST',
     headers: {
         "Content-Type": "application/json",
     },
     body: JSON.stringify({name: 'carlos', avatar: 'yes' })
   })
-  const data = await response.json()
-}
-
-window.addEventListener('load', async () => {
-  console.log('hi there')
-  debugger
+  const data = await responseAddPlayer.json()
   const body = silk(document.getElementsByTagName('body')[0])
+  let addMessage = null
+  setInterval(async () => {
+    debugger
+    const response = await fetch('http://localhost:3000/broadcast')
+    const data = await response.json()
+    console.log(data)
+  }, 1000)
+  body.addChild(
+    silk('div', {},
+      silk('h1', {}, 'Isla Calavera'),
+      silk('ul', {}, add => addMessage = add)
+    )
+  )
 });
